f5e1fffa053223ea74ea05e0fd0857e1f6244c91
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-05-22 18:19:11 +0700

    m

--- MainIA/IBU.h
+++ MainIA/IBU.h
@@ -8,7 +8,7 @@ public :
 	vector<anak*> daftar_anak;
 
 	ibu(string pNama) :nama(pNama) {
-		cout << "Ibu \"" << nama << "\" ada\n";
+		cout << "Ibu \"" << nama << "\" ada\n"; 
 	}
 	~ibu() {
 		cout << "Ibu \"" << nama << "\" tidak ada\n";
